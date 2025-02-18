bebb811fbff215f0ab4897400a9f04c6663bb042
paula-natalia <moraperezpaula@coadvil.edu.co>
2025-02-18 11:41:44 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+dia1=float(input("ingrese las horas de vuelo dia1 "))
+dia2=float(input("ingrese las horas de vuelo dia2 "))
+dia3=float(input("ingrese las horas de vuelo dia3 "))
+dia4=float(input("ingrese las horas de vuelo dia4 "))
+dia5=float(input("ingrese las horas de vuelo dia5 "))
+dia6=float(input("ingrese las horas de vuelo dia6 "))
+dia7=float(input("ingrese las horas de vuelo dia7 "))
+dia8=float(input("ingrese las horas de vuelo dia8 " ))
+dia9=float(input("ingrese las horas de vuelo dia9 "))
+dia10=float(input("ingrese las horas de vuelo dia10 "))
+promedio=(dia1+dia2+dia3+dia4+dia5+dia6+dia7+dia8+dia9+dia10)/10
+print("el promedio de horas de vuelo es", promedio)
