a337755f0e1a67a66c803a4446e8457eac7895ea
paula-natalia <moraperezpaula@coadvil.edu.co>
2025-03-04 11:36:05 -0500

    Add files via upload

---
+++ ejercicios 2.py
@@ -0,0 +1,5 @@
+radio = float(input("ingrese el radio del circulo"))
+perimetro = 2 * 3.14 * radio
+area = 3.14 * radio ** 2
+print("El perimetro del circulo es: ", perimetro)
+print("El area del circulo es: ", area) 
