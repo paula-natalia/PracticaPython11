6fa666dbf24335d05f9ac7c4f71f756cb1d725d9
paula-natalia <moraperezpaula@coadvil.edu.co>
2025-03-04 11:36:38 -0500

    Add files via upload

---
+++ 3ejercicios3.py
@@ -0,0 +1,6 @@
+nota=float(input("Ingrese la primera nota: "))
+nota=float(input("Ingrese la segunda nota: "))
+nota=float(input("Ingrese la tercera nota: "))
+nota=float(input("Ingrese la cuarta nota: "))
+promedio=(nota1+nota2+nota3+nota4)/4
+print("El promedio es: ",promedio)
